ca150009af2606021f851c67e2e2d715c0c53bdf
Vincent Kohm <vini.kohm@gmx.de>
2023-03-29 17:00:19 +0200

    Help section implemented. Now hopefully not pushing a binary...

--- src/main/java/softwaredesign/SystemCommands.java
+++ src/main/java/softwaredesign/SystemCommands.java
@@ -37,7 +37,45 @@ public class SystemCommands {
     }
 
     public void help(){
-        //
+        System.out.println("The user can mine different information from the github repo by issuing command-line instructions following this syntax.\n" +
+                "In order to use the ranking option adhere to the following usage:\n" +
+                "\n" +
+                "Usage:\n" +
+                "  ranking <command> <subcommand>\n" +
+                "  stats <command> <subcommand>\n" +
+                "\n" +
+                "where command and subcommand are optional parameters.\n" +
+                "\n" +
+                "  ranking \t\t\t\t- rank all possible information\n" +
+                "  stats \t\t\t\t- print all statistics\n" +
+                "\n" +
+                "Available commands and sub commands for ranking:\n" +
+                "  commits \t\t\t\t- rank commits based on churn and recent\n" +
+                "  commits churn \t\t- rank commits based on churn\n" +
+                "  commits recent \t\t- rank commits based on recent\n" +
+                "  contributor \t\t\t- rank contributors based on commits, weekend, time, and weekday\n" +
+                "  contributor commits \t- rank contributors based on commit\n" +
+                "  contributor weekend \t- rank contributors based on weekends\n" +
+                "  contributor time \t\t- rank contributors based on time\n" +
+                "  contributor weekday \t- rank contributors based on weekday\n" +
+                "\n" +
+                "Available commands for stats:\n" +
+                "  commits \t\t\t\t- print stats of commits\n" +
+                "  contributors \t\t\t- print stats of contributors\n" +
+                "\n" +
+                "Available system commands:\n" +
+                "  history \t\t\t\t- prints history of all commands\n" +
+                "  help \t\t\t\t\t- prints possible commands\n" +
+                "  restart \t\t\t\t- restarts the program\n" +
+                "  quit \t\t\t\t\t- quits the program\n" +
+                " \n" +
+                "Examples:\n" +
+                "  $ ranking commits\n" +
+                "  $ rankin contributor commits\n" +
+                "  $ stats commits\n" +
+                "  $ ranking\n" +
+                "  $ stats contributor\n" +
+                "  $ history\n");
     }
 
     public void history(History history){
