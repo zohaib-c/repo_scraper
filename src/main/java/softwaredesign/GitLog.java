c8590dc8ab3d19e2bfbac119645ec0202fdd09cc
Zohaib Zaheer <zohaibzaheer99@gmail.com>
2023-03-31 15:12:26 +0200

    Fixed the int parsing error in GitLog (hopefully)

--- src/main/java/softwaredesign/GitLog.java
+++ src/main/java/softwaredesign/GitLog.java
@@ -4,18 +4,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class GitLog extends Application{
-    private LocalDateTime cloneDate;
     private final List<GitCommit> commits;
 
     public GitLog(){
-        cloneDate = LocalDateTime.now();
         this.commits = new ArrayList<>();
     }
 
@@ -36,12 +33,23 @@ public class GitLog extends Application{
         int additions = 0;
         int deletions = 0;
 
+        System.out.println("DEBUG: " + Arrays.toString(statsParts));
         for (int i = 0; i < statsParts.length; i++){
             if (i % 3 == 0) {
-                additions += Integer.parseInt(statsParts[i]);
+                try {
+                    additions += Integer.parseInt(statsParts[i]);
+                }
+                catch (NumberFormatException e){
+                    additions += 0;
+                }
             }
             else if (i % 3 == 1){
-                deletions += Integer.parseInt(statsParts[i]);
+                try {
+                    deletions += Integer.parseInt(statsParts[i]);
+                }
+                catch (NumberFormatException e){
+                    deletions += 0;
+                }
             }
         }
 
@@ -49,7 +57,7 @@ public class GitLog extends Application{
     }
 
     public Boolean runGitLog(String dir){
-        Boolean logSuccesful = Boolean.FALSE;
+        Boolean logSuccessful = Boolean.FALSE;
         String command = "git log --pretty=format:--BEGIN--\n%an\t%at\t%s --numstat";
         ProcessBuilder processBuilder = new ProcessBuilder(command.split(" "));
 
@@ -60,21 +68,27 @@ public class GitLog extends Application{
             int exitCode = process.waitFor();
 
             if (exitCode == 0) {
-                logSuccesful = Boolean.TRUE;
+                logSuccessful = Boolean.TRUE;
                 BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
                 StringBuilder singleCommit = new StringBuilder();
                 String line;
                 while ((line = reader.readLine()) != null) {
                     if (line.equals("--BEGIN--")){
                         line = reader.readLine();
-                        while (!Objects.equals(line, "") && line != null){
+                        while (!Objects.equals(line, "--BEGIN--") && line != null){
+                            if (line.equals("")){
+                                break;
+                            }
                             singleCommit.append(line).append("\t");
                             line = reader.readLine();
                         }
                     }
+                    if (singleCommit.length() != 0){
+                        commits.add(parseCommit(singleCommit.toString()));
+//                        System.out.println(Arrays.toString(singleCommit.toString().split("\t")));
+                        singleCommit = new StringBuilder();
+                    }
 
-                    commits.add(parseCommit(singleCommit.toString()));
-                    singleCommit = new StringBuilder();
                 }
             } else {
                 throw new RuntimeException("git log failed with exit code: " + exitCode);
@@ -82,6 +96,6 @@ public class GitLog extends Application{
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-    return logSuccesful;
+    return logSuccessful;
     }
 }
